4df97ddc7d5248de8b028ea7c73d92c62aa4f084
gorkalombar <gorkalombar@users.noreply.github.com>
2021-10-10 11:37:59 +0200

    dfg

--- eda21/src/eda21/Menu.java
+++ eda21/src/eda21/Menu.java
@@ -1,3 +1,5 @@
+package eda21;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,5 +34,30 @@ public class Menu {
 		 entrada.close();
 		 }
 		 catch(IOException e) {e.printStackTrace();}
+	}
+	
+	public void mostrarMenuPrincipal() {
+		Scanner keyboard = new Scanner(System.in);		
+		
+		System.out.println("MENU PRINCIPAL");
+		System.out.println("Seleccione una de las siguiente opciones:");
+		System.out.println("-0: Cargar los datos");
+		System.out.println("-1: Buscar actor/actriz");
+		System.out.println("-2: Añadir actor/actriz");
+		System.out.println("-3: Obetener las peliculas de un actor/actriz");
+		System.out.println("-4: Obtener los actores/actrices de una pelicula");
+		System.out.println("-5: Incrementar la recaudacion de una pelicula");
+		System.out.println("-6: eliminar actor/actriz");
+		System.out.println("-7: Guardar los datos en un fichero");
+		System.out.println("-8: Obtener lista ordenada de actores");
+		System.out.println("-9: Finalizar programa");
+		System.out.println("->");
+		
+		int selec = keyboard.nextInt();
+		
+		if (selec<0 && selec>9) {
+			System.out.println("Introduzca un numero válido.");
+			this.mostrarMenuPrincipal();
 		}
+	}
 }
