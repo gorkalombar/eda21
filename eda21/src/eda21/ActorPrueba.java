c9a08f6a177e46f638014d7684c0f585dbc9ad1c
andre <andre@10.109.136.166>
2021-10-11 18:51:30 +0200

    sdf

--- eda21/src/eda21/ActorPrueba.java
+++ eda21/src/eda21/ActorPrueba.java
@@ -1,6 +1,5 @@
 //package eda21;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
@@ -10,26 +9,46 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ActorPrueba {
-	Actor a1;
+	static Actor a1;
+	static Actor a2;
+	static Actor a3;
+	static Pelicula p1;
+	static Pelicula p2;
+	static Pelicula p3;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		a1=new Actor("Andrea", "Lopez");
+		a2=new Actor("Benito", "Camela");
+		a3=new Actor("Unachi", "Camala");
+		
+		p1=new Pelicula("Señoras feten");
+		p2=new Pelicula("TLTR");
+		p3=new Pelicula("Star Wars");
+		
+		a1.anadirPeli(p1);
+		a2.anadirPeli(p1);
+		a2.anadirPeli(p2);
+		a3.anadirPeli(p1);
+		a3.anadirPeli(p3);
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		a1=null;
+		a2=null;
+		a3=null;
+		
+		p1=null;
+		p2=null;
+		p3=null;
+		
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
 
 	@Test
 	void testDevolverPelisDelActor() {
+		
 		fail("Not yet implemented");
 	}
 
@@ -40,7 +59,7 @@ class ActorPrueba {
 
 	@Test
 	void testGetApellido() {
-		fail("Not yet implemented");
+		assertEquals("Lopez",a1.getApellido());
 	}
 
 	@Test
