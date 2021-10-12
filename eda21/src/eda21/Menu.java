e6a8fd48f9be1b96ec84fb526b742547f23f760a
gorkalombar <gorkalombar@users.noreply.github.com>
2021-10-12 13:45:23 +0200

    nmb

--- eda21/src/eda21/Menu.java
+++ eda21/src/eda21/Menu.java
@@ -23,13 +23,15 @@ public class Menu {
 		 Scanner entrada = new Scanner(new FileReader(nomF));
 
 		 String linea;
-		 String titulo;
 		 ArrayList<Actor> actores;
 		 String[] arrayDatos;
 		 while (entrada.hasNext()) {
 			 linea = entrada.nextLine();
-			 arrayDatos=linea.split("--->>>");
-
+			 arrayDatos = linea.split("--->>>");
+			 Pelicula peli= new Pelicula(arrayDatos[0]);
+			 while(arrayDatos[].hasNext()) {
+				 
+			 }
 		 }
 		 entrada.close();
 		 }
