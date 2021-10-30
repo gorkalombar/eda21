44694250ceb2116635b3fb5cb238a7a323ce0859
gorkalombar <gorkalombar@users.noreply.github.com>
2021-10-30 17:43:31 +0200

    anadidos los ordered

---
+++ eda21/src/practica2/OrderedListADT.java
@@ -0,0 +1,10 @@
+package segundaFase;
+
+public interface OrderedListADT<T>  extends ListADT<T> {
+	
+	public void add(T elem);
+	// Añade un elemento a la lista (en el lugar de orden que le corresponde)
+
+	public void merge(DoubleLinkedList<T> zerrenda);
+
+}
