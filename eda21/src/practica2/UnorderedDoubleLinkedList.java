0cfbc1f88caf4e5839c19ed0f60990c74ba834f6
Ainhize14 <62991675+Ainhize14@users.noreply.github.com>
2021-10-31 20:34:28 +0100

    cambios en unorderedlinkedlist

--- eda21/src/practica2/UnorderedDoubleLinkedList.java
+++ eda21/src/practica2/UnorderedDoubleLinkedList.java
@@ -5,13 +5,25 @@ public class UnorderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements
 	public void addToFront(T elem) {
 	// añade un elemento al comienzo
 	// coste constante
-
+		Node<T> nuevo= new Node<T>();
+		nuevo.setElement=elem;
+		if (last==null) {
+			last=nuevo;
+		}else {
+			
+		}
 	}
 
 	public void addToRear(T elem) {
 	// añade un elemento al final 
 	// constante constante
-
+		Node<T> nuevo= new Node<T>();
+		nuevo.setElement=elem;
+		if (last==null) {
+			last=nuevo;
+		}else {
+			
+		}
 	}
 	
 	public void addAfter(T elem, T target) {
